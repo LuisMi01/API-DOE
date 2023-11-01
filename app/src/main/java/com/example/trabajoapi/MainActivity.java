2bfc89d6adee3f4834200ed2c066f66121530830
The Chief <lurbevil@myuax.com>
2023-11-01 18:21:38 +0100

    Primer Activity funcionando correctamente

--- app/src/main/java/com/example/trabajoapi/MainActivity.java
+++ app/src/main/java/com/example/trabajoapi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.trabajoapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import android.widget.ImageView;
 
 import android.os.Bundle;
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imagen_principal);
+        String urlImagen = "https://png.pngtree.com/png-clipart/20210404/original/pngtree-metal-bitcoin-png-image_6200465.jpg";
+
+        Glide.with(MainActivity.this)
+                .load(urlImagen).apply(new RequestOptions().centerCrop())
+                .into(imageView);
+
     }
+
+
+
+
 }
